ebf2a97a1928280dadb798b627ea054c51a015ab
cbcc1234 <cbcc1234@naver.com>
2018-07-26 10:55:02 +0900

    Now you can delete comment

--- blog/views.py
+++ blog/views.py
@@ -61,12 +61,14 @@ def post_remove(request, pk):
     post.delete()
     return redirect('post_list')
 
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
+            comment.author = request.user
             comment.post = post
             comment.save()
             return redirect('post_detail', pk=post.pk)
@@ -85,3 +87,5 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
+
+
